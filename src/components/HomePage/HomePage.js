f037ca18557ab18d794fab94c064d9c56df67160
Alex Liu <liuzhendell@gmail.com>
2021-01-30 16:27:01 -0700

    setup reducer,router

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const HomePage=()=>{
+
+
+
+    return(
+        <>
+            this is homepage
+
+        </>
+    )
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import {Provider} from 'react-redux';
 import reportWebVitals from "./reportWebVitals";
-import "./bootstrap.min.css";
+import configureStore from "./configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
